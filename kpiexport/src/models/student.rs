6cb54e2f5facb2f97d8a7a3ad28f14452a146b02
Nikita Volobuev <nikitavbv@gmail.com>
2021-04-28 23:58:42 +0300

    feat(models): transliterate names when comparing

--- kpiexport/src/models/student.rs
+++ kpiexport/src/models/student.rs
@@ -1,5 +1,8 @@
 use levenshtein::levenshtein;
+use translit::{Gost779B, ToLatin, Language};
+
 
 fn name_distance(a: &str, b: &str) -> usize {
-    levenshtein(a, b)
+    let trasliterator = Gost779B::new(Language::Ru);
+    levenshtein(transliterator.to_latin(a), transliterator.to_latin(b))
 }
