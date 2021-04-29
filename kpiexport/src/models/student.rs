66d24f1882900e24c5d04b9abd31c115f2c0c201
Nikita Volobuev <nikitavbv@gmail.com>
2021-04-29 23:53:01 +0300

    fix(kpiexport): fix build error

--- kpiexport/src/models/student.rs
+++ kpiexport/src/models/student.rs
@@ -4,5 +4,5 @@ use translit::{Gost779B, ToLatin, Language};
 
 fn name_distance(a: &str, b: &str) -> usize {
     let trasliterator = Gost779B::new(Language::Ru);
-    levenshtein(transliterator.to_latin(a), transliterator.to_latin(b))
+    levenshtein(&trasliterator.to_latin(a), &trasliterator.to_latin(b))
 }
