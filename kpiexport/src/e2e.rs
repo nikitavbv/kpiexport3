51d560b978fafe63e40b4d7a484cdb7e56437680
Nikita Volobuev <nikitavbv@gmail.com>
2021-09-10 08:47:20 +0300

    fix(e2e): fix personal schedule test

--- kpiexport/src/e2e.rs
+++ kpiexport/src/e2e.rs
@@ -5,15 +5,9 @@ mod tests {
     
     #[tokio::test]
     async fn get_personal_schedule() {
-        // TODO: fix this test
-        let res = reqwest::get("https://kpiexport.nikitavbv.com/schedule/%D0%86%D0%9F-82?lastName=Volobuev").await
+        let res = reqwest::get("https://kpiexport.nikitavbv.com/v1/schedule/%D0%86%D0%9F-82?lastName=Volobuev").await
             .unwrap();
         let response_status = &res.status();
-        let response_text = res.text().await.unwrap();
-
-        println!("result text is: {:?}", response_text);
-        println!("result is {:?}", response_status);
-
         assert_eq!(response_status, &StatusCode::OK);
     }
 }
