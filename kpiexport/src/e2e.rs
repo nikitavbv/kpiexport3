4f0573a784363101b20aacd04adc29877ecc5f13
Nikita Volobuev <nikitavbv@gmail.com>
2021-10-16 23:40:11 +0300

    feat(tests): reenable e2e tests

--- kpiexport/src/e2e.rs
+++ kpiexport/src/e2e.rs
@@ -2,11 +2,11 @@
 mod tests {
     use reqwest::StatusCode;
 
-    /*#[tokio::test]
+    #[tokio::test]
     async fn get_personal_schedule() {
         let res = reqwest::get("https://kpiexport.nikitavbv.com/v1/schedule/%D0%86%D0%9F-82?lastName=Volobuev").await
             .unwrap();
         let response_status = &res.status();
         assert_eq!(response_status, &StatusCode::OK);
-    }*/
+    }
 }
