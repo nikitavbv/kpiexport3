23c422acc28705c3771822d41fd86a1fb8bb1a22
Nikita Volobuev <nikitavbv@gmail.com>
2022-01-30 23:57:52 +0200

    feat(kpiexport): move is_hot_month to utils

--- kpiexport/src/utils.rs
+++ kpiexport/src/utils.rs
@@ -1,3 +1,4 @@
+use chrono::{DateTime, Utc};
 use crate::errors::RozkladParseError;
 
 const VIEW_SCHEDULE_PREFIX: &'static str = "ViewSchedule.aspx?g=";
@@ -7,4 +8,10 @@ pub fn group_id_from_url(url: &str) -> Result<String, RozkladParseError> {
         None => Err(RozkladParseError::FailedToParseGroupId {}),
         Some(index) => Ok(url[index + VIEW_SCHEDULE_PREFIX.len()..].to_string())
     }
+}
+
+pub fn is_hot_month() -> bool {
+    // in January, February, August and September refresh every 2-3 days, other months - every ~20 days
+    let utc: DateTime<Utc> = Utc::now();
+    utc.month0() <= 1 || utc.month0() == 7 || utc.month0() == 8
 }
