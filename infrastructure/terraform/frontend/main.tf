129113dbcb661e124146c0a5a3fcd0b09bf9d92a
Nikita Volobuev <nikitavbv@gmail.com>
2022-02-05 23:59:18 +0200

    feat(infrastructure): add terraform for frontend

---
+++ infrastructure/terraform/frontend/main.tf
@@ -0,0 +1,57 @@
+terraform {
+    backend "gcs" {
+        bucket = "nikitavbv_tf_state"
+        prefix = "manticore/kpiexport-frontend"
+    }
+}
+
+provider "google" {
+    project = "nikitavbv"
+    region = "europe-central2"
+}
+
+provider "google-beta" {
+    project = "nikitavbv"
+    region = "europe-central2"
+}
+
+variable "service_version" {
+    type = string
+    description = "Version of image to deploy"
+    default = "0.1.76"
+}
+
+resource "google_cloud_run_service" "kpiexport_service" {
+    provider = google-beta
+
+    name = "kpiexport"
+    location = "europe-central2"
+
+    autogenerate_revision_name = true
+
+    metadata {
+        annotations = {
+            generated-by = "magic-modules"
+            "run.googleapis.com/ingress" = "all"
+            "run.googleapis.com/launch-stage" = "BETA"
+        }
+    }
+
+    template {
+        metadata {
+            annotations = {
+                "autoscaling.knative.dev/maxScale" = "5"
+                "run.googleapis.com/vpc-access-connector" = "projects/nikitavbv/locations/europe-central2/connectors/cloud-run-api-connector-w"
+                "run.googleapis.com/vpc-access-egress"    = "all-traffic"
+            }
+        }
+
+        spec {
+            service_account_name = "916750455653-compute@developer.gserviceaccount.com"
+    
+            containers {
+                image = "eu.gcr.io/nikitavbv/nikitavbv/kpiexport:${var.service_version}"
+            }
+        }
+    }
+}
